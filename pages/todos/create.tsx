9bfcb4cb0f4c5dc9eaac6a731f8a7a69566b3c77
samynaj <samuelnnj@gmail.com>
2022-04-08 08:50:20 +0100

    create todo page

--- pages/todos/create.tsx
+++ pages/todos/create.tsx
@@ -20,34 +20,39 @@ function Create(props: CreateProps) {
     const handleSubmit: FormEventHandler<HTMLFormElement> = async event => {
         event.preventDefault()
 
-        const d = new Date()
+        const date = new Date()
+        const d = date.getDate()
+        const m = date.getMonth() + 1
+        const y = date.getFullYear()
 
         const todo: Todo = {
             title,
             completed: false,
             body,
-            date: `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`
+            date: `${y}-${m <= 9 ? '0' + m : m}-${d <= 9 ? '0' + d : d}`
         }
 
         await fetch(props.url, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(todo),
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(todo),
         })
-
+        
         router.push("/")
     }
 
     return (
-        <div>
-        <h1>Create a New Todo</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
-            <input type="text" placeholder="What to do" onChange={(e) => setBody(e.target.value)} />
-            <button type="submit">Create todo</button>
-        </form>
+        <div className='container min-h-screen flex flex-col items-center '>
+            <div className="w-full h-80 py-5 pt-py-10 px-5 flex flex-col items-center justify-center gap-5 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <h1 className="text-2xl font-bold">Create a New Todo</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center w-full">
+                <input className="w-full p-2 h-11 rounded-lg text-black sm:w-1/2 focus:outline-none" type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
+                <input className="w-full p-2 h-11 rounded-lg text-black sm:w-1/2 focus:outline-none" type="text" placeholder="What to do" onChange={(e) => setBody(e.target.value)} />
+                <button type="submit" disabled={title === '' && body === ''} className='bg-blue-500 w-52 rounded-lg p-2 font-semibold hover:scale-110'>Create todo</button>
+            </form>
+            </div>
         </div>
     )
 }
